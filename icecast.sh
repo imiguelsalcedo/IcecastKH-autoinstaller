873ff88bfe4c6b8d344140e87c417a7740cf79c9
Miguel Salcedo <imiguelsalcedo@hotmail.com>
2015-11-28 18:56:35 -0500

    test4

--- icecast.sh
+++ icecast.sh
@@ -43,8 +43,9 @@ chown -R icecast:icecast /var/run/icecast
 
 
 echo -e "\n --------icecast.xml--------\n"
-cp /usr/src/icecast/icecast-kh-master/examples/icecast.xml /usr/local/etc/
+cp /usr/src/icecast/icecast-kh-master/examples/icecast.xml /usr/local/etc/icecast.bak.xml
 cd /usr/local/etc/
+wget https://puntohost.com/files/icecast.xml
 nano icecast.xml
 
 echo -e "Start server= /usr/local/bin/icecast -c /usr/local/etc/icecast.xml -b"
